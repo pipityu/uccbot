de2823461dfb539a1be0943f45d24411db11d047
pipityu <peti.contact@gmail.com>
2019-10-27 15:49:36 +0100

    bot

--- src/main/resources/static/js/datepicker.js
+++ src/main/resources/static/js/datepicker.js
@@ -1,13 +1,14 @@
 function addDate() {
 
     var xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("GET", "\"https://api.manychat.com/fb/subscriber/getInfo?subscriber_id=3809668825726118", true);
+    xmlhttp.open("GET", "https://api.manychat.com/fb/subscriber/getInfo?subscriber_id=3809668825726118", true);
     xmlhttp.setRequestHeader("Authorization", "Bearer 105197630914532:ba342569ac0c5408909eee97f971b9a6");
     xmlhttp.setRequestHeader("Content-Type", "application/json");
     xmlhttp.onload = function () {
         if (this.readyState == 4 && this.status == 200) {
             var myObj = JSON.parse(this.responseText);
             document.getElementById("leker").innerHTML = myObj.first_name;
+            alert("ide jutott");
         }
     };
     xmlhttp.send();
