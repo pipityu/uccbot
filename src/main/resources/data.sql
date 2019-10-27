21be071759a2a0ea52557931117ddbc3867866ec
pipityu <peti.contact@gmail.com>
2019-10-27 11:11:06 +0100

    bot

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -11,15 +11,15 @@ delete from  users;
 
 INSERT INTO roles (id, name) VALUES
 (1, 'ROLE_ADMIN'),
-(2, 'ROLE_ACTUATOR'),
-(3, 'ROLE_USER');
+--(2, 'ROLE_ACTUATOR'),
+(2, 'ROLE_USER');
 
 INSERT INTO users (id, email, password, name) VALUES
-(1, 'admin@gmail.com', '$2a$10$hKDVYxLefVHV/vtuPhWD3OigtRyOykRLDdUAp80Z1crSoS1lFqaFS', 'Admin'),
-(3, 'user@gmail.com', '$2a$10$ByIUiNaRfBKSV6urZoBBxe4UbJ/sS6u1ZaPORHF9AtNWAuVPVz1by', 'User');
+(1, 'admin@admin.com', '$2a$10$hKDVYxLefVHV/vtuPhWD3OigtRyOykRLDdUAp80Z1crSoS1lFqaFS', 'Admin'),
+(2, 'user@user.com', '$2a$10$ByIUiNaRfBKSV6urZoBBxe4UbJ/sS6u1ZaPORHF9AtNWAuVPVz1by', 'User');
 
 insert into user_role(user_id, role_id) values
 (1,1),
 (1,2),
-(1,3),
-(3,2);
+--(1,3),
+(2,2);
