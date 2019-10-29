a5991e29403d391d0f302a88b7d809001b45a45f
pipityu <peti.contact@gmail.com>
2019-10-29 09:53:46 +0100

    bot

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -16,9 +16,10 @@ INSERT INTO roles (id, name) VALUES
 
 INSERT INTO requests(id, name, type, start_date, end_date, status)  VALUES
 (1, 'user@user.com', 'szabadság', '2019-01-01', '2019-01-11', 'várakozás');
+(2, 'admin@admin.com', '0', '0', '0', '0');
 
 INSERT INTO users (id, email, password, name, request_id) VALUES
-(1, 'admin@admin.com', '$2a$10$hKDVYxLefVHV/vtuPhWD3OigtRyOykRLDdUAp80Z1crSoS1lFqaFS', 'Admin'),
+(1, 'admin@admin.com', '$2a$10$hKDVYxLefVHV/vtuPhWD3OigtRyOykRLDdUAp80Z1crSoS1lFqaFS', 'Admin',2),
 (2, 'user@user.com', '$2a$10$ByIUiNaRfBKSV6urZoBBxe4UbJ/sS6u1ZaPORHF9AtNWAuVPVz1by', 'User', 1);
 
 insert into user_role(user_id, role_id) values
