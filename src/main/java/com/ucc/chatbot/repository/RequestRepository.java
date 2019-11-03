16d806610abb992b6a1b1d6e329930fdf0be7923
pipityu <peti.contact@gmail.com>
2019-11-03 20:30:18 +0100

    bot

--- src/main/java/com/ucc/chatbot/repository/RequestRepository.java
+++ src/main/java/com/ucc/chatbot/repository/RequestRepository.java
@@ -8,7 +8,7 @@ public interface RequestRepository extends JpaRepository<Request, Integer> {
     @Query(value = "select * from requests where username =?", nativeQuery = true)
         public Request findByUserName(String username);
 
-    @Query(value = "update requests set type=? start_date=? end_date=? status=? where username=?", nativeQuery = true)
+    @Query(value = "update requests set type=?, start_date=?, end_date=?, status=? where username=?", nativeQuery = true)
         public void updateRequest(String type, String start_date, String end_date, String status, String username);
 
 }
