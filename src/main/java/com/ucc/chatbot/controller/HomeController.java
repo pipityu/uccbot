04c4ac9cf2067df5712e7c0c1d9e612265c19362
pipityu <peti.contact@gmail.com>
2019-11-05 18:24:29 +0100

    bot

--- src/main/java/com/ucc/chatbot/controller/HomeController.java
+++ src/main/java/com/ucc/chatbot/controller/HomeController.java
@@ -135,7 +135,7 @@ public class HomeController {
     //action = formról beérkező adat(elutasítás vagy elfogadás)
     //id = szintén a formról a request id-ja
     @PostMapping("/request/response")
-    public String response(@RequestBody String action, int requestId) {
+    public String response(@RequestParam String action, @RequestParam int requestId) {
       //  int id = Integer.parseInt(stringId);
         Optional<Request> request = reqservice.findRequestById(requestId);     //OPTIONAL egy generikus tároló 0,1 értékekkel ami azt nézi hogy létezik e az elem(hibakezelésre szolgál)
         Request r = request.get();      //get() ha létezik az elem akkor visszaadja az értékét ha nem akkor NoSuchElementException-t dob
